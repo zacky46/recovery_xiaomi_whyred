ca8097de0054b7085dd04736ef0a5a3b1cc0801d
Sushrut Gupta <guptasushrut@gmail.com>
2021-05-24 14:34:36 +0000

    Add Build Vars

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,99 @@
-add_lunch_combo omni_whyred-eng
-add_lunch_combo omni_whyred-userdebug
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="whyred"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	
+	export FOX_R11=1
+	export OF_PATCH_AVB20=1
+    export OF_SCREEN_H=2160
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_FBE_METADATA_MOUNT_IGNORE=1
+	export FOX_DELETE_AROMAFM=1
+    export OF_USE_SYSTEM_FINGERPRINT=1
+    export OF_CHECK_OVERWRITE_ATTEMPTS=1
+
+    # About Settings
+    export OF_MAINTAINER="Sushrut Gupta"
+    export FOX_VERSION="R11.1_0"
+    export FOX_BUILD_TYPE="Beta"
+
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_ZIP_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export OF_USE_GREEN_LED=1
+
+    OF_TARGET_DEVICES="whyred,whyredin"
+    OF_USE_TWRP_SAR_DETECT="1"
+
+	# OTA for custom ROMs
+        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+
+	# quick backup defaults
+        export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	export LC_ALL="C"
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+
+	add_lunch_combo omni_"$FDEVICE"-eng
+	add_lunch_combo omni_"$FDEVICE"-userdebug
+fi
+#
